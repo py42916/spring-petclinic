456e02520c99820f6df3c17ec90007b389d7ee9a
Paul Young <thepaulayoung@gmail.com>
2023-02-10 15:54:04 -0800

    Initial version of data model POJO classes.

---
+++ src/main/java/com/example/springpetclinic/model/Person.java
@@ -0,0 +1,15 @@
+package com.example.springpetclinic.model;
+
+public class Person {
+
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
